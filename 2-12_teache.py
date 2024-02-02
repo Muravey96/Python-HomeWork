912bfabdd6efc71255f56bf3e681ad5a38a13256
Muravey96 <114182631+Muravey96@users.noreply.github.com>
2024-02-02 16:46:36 +0300

    Update 2-12_teache.py

--- 2-12_teache.py
+++ 2-12_teache.py
@@ -41,3 +41,16 @@ while num_1 < p:
         print(num_1, num_2)
         break
     num_1 += 1
+
+
+# --------------- 3 вар
+# ------------------3 вариант
+solutions = []
+for i in range(1, 1001):
+    for j in range(1, 1001):
+        if s == i + j and p == i * j:
+            solutions.append((min(i, j), max(i, j)))
+solutions = list(set(solutions))
+
+for solution in solutions:
+    print(solution[0], solution[1])
